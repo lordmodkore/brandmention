710c54a48bd0fddbe7a21a05d447098448d2f1f6
Elland <elland.pansensoy@gmail.com>
2017-09-14 22:20:32 +0800

    added elixir mix

--- trunk/webpack.mix.js
+++ trunk/webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
-
+var elixir = require('laravel-elixir');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,10 +11,22 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.js('resources/assets/js/main.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
    
 // mix.browserSync({
 //     proxy: 'brandmention.com'
 // });
+
+mix.scripts([
+    './bower_components/jquery/dist/jquery.js',
+    './bower_components/bootstrap/dist/js/bootstrap.js',
+    './resources/assets/js/apps/*.js',
+    './resources/assets/js/charts/*.js',
+    './resources/assets/js/forms/*.js',
+    './resources/assets/js/helpers/*.js',
+    './resources/assets/js/maps/*.js',
+    './resources/assets/js/tables/*.js',
+    './resources/assets/js/ui/*.js',
+], 'public/js/vendor.js');
