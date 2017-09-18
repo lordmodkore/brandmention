eb76932a54cf5d1f449f2562d8e1b586db0096d6
Elland <elland.pansensoy@gmail.com>
2017-09-18 08:50:24 +0800

    added changes

--- trunk/app/Http/Controllers/UserController.php
+++ trunk/app/Http/Controllers/UserController.php
@@ -14,6 +14,12 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    
+    public function __construct(){
+
+        $this->middleware('auth');
+    }
+    
     public function index()
     {
         return view('users.index');
